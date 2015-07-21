d077c911d9401821f423587fdf13223e333928d1
huangfeng <850875418@qq.com>
2015-07-21 22:15:21 +0800

    aaa

--- mulinson/js/main.js
+++ mulinson/js/main.js
@@ -2,11 +2,18 @@ window.onload = function(){
 	var back_top =document.getElementById('back_top');
     var btn = document.getElementById('btn');
     var timer = null;
+
+    var text_input = document.getElementById('demo_ul');
+    var  text_val = document.getElementById('text_val');
+    var sub = document.getElementById('submit');
+
     // 获取页面的可视高度
     var clientHeight = document.documentElement.clientHeight;
     var isTop = true;
+
     // 滚动条滚动后触发
     window.onscroll = function (){
+
     	// 获取滚动条滚动的就高度
     	var slTop = document.documentElement.scrollTop || document.body.scrollTop;
         if(slTop >= clientHeight){
@@ -14,6 +21,7 @@ window.onload = function(){
         } else {
         	back_top.style.display = 'none';
         }
+
         // 当滚动条滚动时清空timer
         if(!isTop){
         	clearInterval(timer);
@@ -21,15 +29,29 @@ window.onload = function(){
         isTop = false;
     }
     btn.onclick = function (){
+
     	// 获取滚动条滚动的高度
     	var slTop = document.documentElement.scrollTop || document.body.scrollTop;
     	timer = setInterval( function(){
             var changespeed = slTop/2
             document.documentElement.scrollTop = document.body.scrollTop = -slTop + changespeed; 
     	},30);
+
     	// 当滚动高度为0时清空timer
         if (!slTop) {
         	clearInterval(timer);
         };
+        
+    }
+    sub.onclick = function(){
+        var val = text_val.value;
+        if(val == ''){
+            return;
+        } else{
+            var li = document.createElement('li');
+            li.innerHTML = val;
+            text_input.appendChild(li);
+            text_val.value = '';
+        }
     }
 }
